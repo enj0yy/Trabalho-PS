03df60acf8465e508cae3c58c96160ab00af280f
Igor-G-D <igorgomesdutra@gmail.com>
2023-11-17 13:45:34 -0300

    modificado comentario na classe memoria

--- src/trabalho/ps/Executor/Memoria.java
+++ src/trabalho/ps/Executor/Memoria.java
@@ -7,6 +7,6 @@ public class Memoria {
     private ArrayList<String> memoria;
 
     Memoria() {
-        memoria = new ArrayList<String>(Collections.nCopies(100, "000")); // faz 100 celulas de memória com o valor "000"
+        memoria = new ArrayList<String>(Collections.nCopies(100, "000")); // faz 100 celulas de memória com o valor "000", representando um hexadecimal de 3 digitos, ou seja 24 bits
     }
 }
